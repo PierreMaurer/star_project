09bc5eec1727cee0c597333208cc54c8c9ffd00b
PierreMaurer <pierre.maurer@epitech.eu>
2021-10-08 22:07:16 +0200

    commit

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ void star_top(int size)
     int star_size = size*3;
     int space = 1;
     printf("%i \n", star_size);
-    for (int a = 0; a < size; a++) {
+    for (a; a < size; a++) {
         for (int b = 0; b < star_size; b++) {
             my_putchar(' ');
         }
