75c5e61817e2be5919a8dd5beeb6f190ad397cf1
PierreMaurer <pierre.maurer@epitech.eu>
2021-10-10 14:16:20 +0200

    mid_star

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 ** display a star based on given size
 */
 #include <stdio.h>
+#include <unistd.h>
 
 void my_putchar(c)
 {
@@ -35,7 +36,79 @@ void star_top(int size)
     }
 }
 
+void bot_star(int size)
+{
+    /* TODO: Finish bot_star */
+    int a = 0;
+    int space = size;
+    int star_size = (size*2)+1;
+    
+    for ( int line = 0; line < size; line++) {
+        for(a = a; a < star_size; a++) {
+            my_putchar(' ');
+        }
+        my_putchar('*');
 
+        if (line < size-1) {
+            for (int c = 0; c <= space; c++) {
+                my_putchar(' ');
+            }
+            my_putchar('*');
+            if (line <= 1) {
+                space-= 2;
+            } else {
+                space --;
+            }
+        }
+        my_putchar('\n');
+        star_size++;
+        a = 0;
+    }
+        
+}
+void mid_star(int size)
+{
+    int count = 0;
+    int space = 1;
+    int star_size = (size*6)-4;
+    for (count = count; count < size; count++) {
+        for (int i = 0; i < space; i++) {
+            my_putchar(' ');
+        }
+        my_putchar('*');
+        space++;
+
+        for (int j = 0; j <= star_size; j++) {
+            my_putchar(' ');
+        }
+        star_size -= 2;
+        my_putchar('*');
+        my_putchar('\n');
+    }
+    mid_second_part(size);
+}
+
+void mid_second_part(int size) {
+    int count = 0;
+    int space = size-1;
+    int star_size = (size*6)-size;
+    
+    for (count = count; count < size-1; count++) {
+        for (int i = 0; i < space; i++) {
+            my_putchar(' ');
+        }
+        my_putchar('*');
+
+        for(int j = 0; j <= star_size-size; j++){
+            my_putchar(' ');
+        }
+        star_size +=2;
+        my_putchar('*');
+        my_putchar('\n');
+        space--;
+    }
+    
+}
 void line_star(int size) {
     int star_size = size*3;
     int line_width = (size*2)+1;
@@ -54,8 +127,17 @@ void line_star(int size) {
     my_putchar('\n');
 
 }
-void main (unsigned int size)
+
+void star (unsigned int size)
 {
-    star_top(3);
-    line_star(3);
+    star_top(size);
+    line_star(size);
+    mid_star(size);
+    line_star(size);
+    bot_star(size);
+}
+
+int main(void)
+{
+    star(5);
 }
